88d05e4e6bdc4bf5676d52f58b3485b00c9a95a6
sandamal <sandamal0088@gmail.com>
2026-03-29 15:45:26 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'roster-gen-v9-cache';
+const urlsToCache = [
+  './index.html',
+  './icon.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
